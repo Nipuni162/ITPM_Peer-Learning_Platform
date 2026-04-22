5b450cdca807f75a2ae69d9a80310b0c657a9545
Ashinsanie535 <g.ashinsaniumanda@gmail.com>
2026-04-22 22:55:33 +0530

    Add ForumPost schema with replies and likes

---
+++ ForumPost.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    text: { type: String, required: true, trim: true },
+  },
+  { timestamps: true }
+);
+
+const forumPostSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true, trim: true },
+    content: { type: String, required: true, trim: true },
+
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+
+    upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [commentSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ForumPost", forumPostSchema);
